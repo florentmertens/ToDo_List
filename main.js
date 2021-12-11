f0c8ff796794120942783a72322af17225bffac2
florentmertens <mertens.florent00@gmail.com>
2021-12-11 23:32:31 +0100

    add todo list

---
+++ main.js
@@ -0,0 +1,72 @@
+const add = document.querySelector('#newTaskSubmit');
+
+const input = document.querySelector('#newTaskInput');
+
+const list = document.querySelector('#listTasks');
+
+const select = document.querySelector('select');
+
+add.addEventListener('click', () => {
+  if (input.value != '') {
+  const value = input.value;
+  const taskDiv = document.createElement('div');
+  taskDiv.className = 'task';
+  const finishDiv = document.createElement('div');
+  finishDiv.id = 'finish';
+  taskDiv.appendChild(finishDiv);
+  const p = document.createElement('p');
+  p.innerHTML = value;
+  taskDiv.appendChild(p);
+  const iCheck = document.createElement('i')
+  iCheck.className = 'fas fa-check';
+  iCheck.id = 'check';
+  taskDiv.appendChild(iCheck);
+  const iTrash= document.createElement('i');
+  iTrash.className = 'fas fa-trash';
+  iTrash.id = 'trash';
+  taskDiv.appendChild(iTrash);
+  input.value = '';
+  listTasks.appendChild(taskDiv);
+  filter();
+  }
+})
+
+window.addEventListener('click', (e) => {
+  if (e.target.id == 'check') {
+    const task = e.target.parentNode;
+    task.classList = 'task disabled';
+    task.children[0].classList = 'check';
+    task.children[1].classList = 'finishP';
+  } else if (e.target.id == 'trash') {
+    e.target.parentNode.remove();
+  }
+})
+
+select.addEventListener('change', () => {
+  filter();
+})
+
+function filter() {
+  const tasks = document.querySelectorAll('.task');
+  if (select.value == 'finish') {
+    for (var i = 0; i < tasks.length; i++) {
+      if (!tasks[i].classList.contains('disabled')) {
+        tasks[i].style.display = 'none';
+      }else {
+        tasks[i].style.display = 'flex';
+      }
+    }
+  }else if (select.value == 'toDo') {
+    for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].classList.contains('disabled')) {
+        tasks[i].style.display = 'none';
+      }else {
+        tasks[i].style.display = 'flex';
+      }
+    }
+  }else {
+    for (var i = 0; i < tasks.length; i++) {
+      tasks[i].style.display = 'flex';
+    }
+  }
+}
